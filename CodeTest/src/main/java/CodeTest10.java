a5722a7067ef1510e56039ce334e294f24044f41
BONOBONOYA1 <116361833+BONOBONOYA1@users.noreply.github.com>
2022-11-22 22:29:01 +0900

    코드 테스트 문제 8번 평균 구하기

---
+++ CodeTest/src/main/java/CodeTest10.java
@@ -0,0 +1,13 @@
+public class CodeTest10 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        double answer = 0;
+        double sum = 0;
+
+        for(int i = 0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        answer = sum / arr.length;
+        System.out.println(answer);
+    }
+}
